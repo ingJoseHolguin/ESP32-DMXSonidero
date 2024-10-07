f2a5f8cb268dda7cd7fc0c6f0f08938dc59da386
ingJoseHolguin <jp.holguin95@gmail.com>
2024-10-06 22:50:37 -0600

    Librerias LCD y touch Correctas

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,32 @@
 #include <Arduino.h>
-#include <TFT_eSPI.h>       // Librería para manejar la pantalla
-#include <XPT2046_Touchscreen.h> // Librería para el touch capacitivo
+#include <SPI.h>
+#include <TFT_eSPI.h>
+#include <XPT2046_Touchscreen.h>
+#include <WiFi.h>
+#include <ArtnetWifi.h>
 
-#define TOUCH_CS 22         // Pin de Chip Select para el touch
-#define TFT_CS 5            // Pin de Chip Select para la pantalla
-#define TFT_RST 4           // Pin de reset de la pantalla
-#define TFT_DC 2            // Pin de datos/comando de la pantalla
 
-TFT_eSPI tft = TFT_eSPI();  // Instancia para la pantalla
-XPT2046_Touchscreen ts(TOUCH_CS);  // Instancia para el touch}
+const char* ssid = "iPhone Pepon";
+const char* password = "1234qwer";
+
+TFT_eSPI tft = TFT_eSPI();
+
+// Touchscreen pins
+#define XPT2046_IRQ 36   // T_IRQ
+#define XPT2046_MOSI 32  // T_DIN
+#define XPT2046_MISO 39  // T_OUT
+#define XPT2046_CLK 25   // T_CLK
+#define XPT2046_CS 33    // T_CS
+
+SPIClass touchscreenSPI = SPIClass(VSPI);
+XPT2046_Touchscreen touchscreen(XPT2046_CS, XPT2046_IRQ);
+
+#define SCREEN_WIDTH 320
+#define SCREEN_HEIGHT 240
+#define FONT_SIZE 2
+
+// Touchscreen coordinates: (x, y) and pressure (z)
+int x, y, z;
 
 TaskHandle_t Task1;
 TaskHandle_t Task2;
@@ -35,56 +53,79 @@ void Task2code( void * parameter ) {
   }
 }
 
+
+// Print Touchscreen info about X, Y and Pressure (Z) on the Serial Monitor
+void printTouchToSerial(int touchX, int touchY, int touchZ) {
+  Serial.print("X = ");
+  Serial.print(touchX);
+  Serial.print(" | Y = ");
+  Serial.print(touchY);
+  Serial.print(" | Pressure = ");
+  Serial.print(touchZ);
+  Serial.println();
+}
+
+// Print Touchscreen info about X, Y and Pressure (Z) on the TFT Display
+void printTouchToDisplay(int touchX, int touchY, int touchZ) {
+  // Clear TFT screen
+  tft.fillScreen(TFT_WHITE);
+  tft.setTextColor(TFT_BLACK, TFT_WHITE);
+
+  int centerX = SCREEN_WIDTH / 2;
+  int textY = 80;
+ 
+  String tempText = "X = " + String(touchX);
+  tft.drawCentreString(tempText, centerX, textY, FONT_SIZE);
+
+  textY += 20;
+  tempText = "Y = " + String(touchY);
+  tft.drawCentreString(tempText, centerX, textY, FONT_SIZE);
+
+  textY += 20;
+  tempText = "Pressure = " + String(touchZ);
+  tft.drawCentreString(tempText, centerX, textY, FONT_SIZE);
+}
+
 void setup() {
   Serial.begin(115200);
 
-    xTaskCreatePinnedToCore(
-    Task1code,   // Función que ejecuta la tarea
-    "Task1",     // Nombre de la tarea
-    10000,       // Tamaño de la pila (en bytes)
-    NULL,        // Parámetro para pasar a la tarea
-    1,           // Prioridad de la tarea
-    &Task1,      // Manejador de la tarea
-    0);          // Núcleo 0
+  // Start the SPI for the touchscreen and init the touchscreen
+  touchscreenSPI.begin(XPT2046_CLK, XPT2046_MISO, XPT2046_MOSI, XPT2046_CS);
+  touchscreen.begin(touchscreenSPI);
+  // Set the Touchscreen rotation in landscape mode
+  // Note: in some displays, the touchscreen might be upside down, so you might need to set the rotation to 3: touchscreen.setRotation(3);
+  touchscreen.setRotation(1);
 
-  // Crear la tarea 2 en el núcleo 1
-  xTaskCreatePinnedToCore(
-    Task2code,   // Función que ejecuta la tarea
-    "Task2",     // Nombre de la tarea
-    10000,       // Tamaño de la pila (en bytes)
-    NULL,        // Parámetro para pasar a la tarea
-    1,           // Prioridad de la tarea
-    &Task2,      // Manejador de la tarea
-    1);          // Núcleo 1
-
-
-  // Inicializa la pantalla
+  // Start the tft display
   tft.init();
-  tft.setRotation(1);  // Configura la orientación de la pantalla
+  // Set the TFT display rotation in landscape mode
+  tft.setRotation(1);
 
-  // Inicializa el touch
-  if (ts.begin()) {
-    Serial.println("Touchscreen initialized");
-  } else {
-    Serial.println("Touchscreen initialization failed");
-  }
+  // Clear the screen before writing to it
+  tft.fillScreen(TFT_WHITE);
+  tft.setTextColor(TFT_BLACK, TFT_WHITE);
+  
+  // Set X and Y coordinates for center of display
+  int centerX = SCREEN_WIDTH / 2;
+  int centerY = SCREEN_HEIGHT / 2;
 
-  // Limpiar la pantalla
-  tft.fillScreen(TFT_BLACK);
+  tft.drawCentreString("Hello, world!", centerX, 30, FONT_SIZE);
+  tft.drawCentreString("Touch screen to test", centerX, centerY, FONT_SIZE);
 }
 
 void loop() {
-  // Verificar si se toca la pantalla
-  if (ts.touched()) {
-    TS_Point p = ts.getPoint();  // Obtener la posición del touch
+  // Checks if Touchscreen was touched, and prints X, Y and Pressure (Z) info on the TFT display and Serial Monitor
+  if (touchscreen.tirqTouched() && touchscreen.touched()) {
+    // Get Touchscreen points
+    TS_Point p = touchscreen.getPoint();
+    // Calibrate Touchscreen points with map function to the correct width and height
+    x = map(p.x, 200, 3700, 1, SCREEN_WIDTH);
+    y = map(p.y, 240, 3800, 1, SCREEN_HEIGHT);
+    z = p.z;
 
-    // Muestra las coordenadas del toque
-    Serial.print("Touch at: ");
-    Serial.print(p.x);
-    Serial.print(", ");
-    Serial.println(p.y);
+    printTouchToSerial(x, y, z);
+    printTouchToDisplay(x, y, z);
 
-    // Dibuja un círculo donde se detectó el toque
-    tft.fillCircle(p.x, p.y, 5, TFT_RED);
+    delay(100);
   }
 }
